bb254b9e4d0ffcdd2aab8c5670c18d60aec29c3e
MuhammadSami42004 <muhammadsami42004@gmail.com>
2024-07-09 02:07:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//  simple calclutor 
+let weight = 30;
+let height = 22;
+let bmi = height / (weight * weight);
+// let bmi = weight/(height*height)
+console.log(`Muhammad Sami Your BMI Is ${bmi}`);
+export {};
